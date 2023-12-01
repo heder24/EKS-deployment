7b7e4529dcc4eb0380d38245339f6c285a451639
heder redon <hederredon@gmail.com>
2023-11-30 23:09:53 -0500

    modified deployment file

--- main.tf
+++ main.tf
@@ -65,9 +65,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     prod = {
-      min_size     = 3
+      min_size     = 6
       max_size     = 10
-      desired_size = 3
+      desired_size = 6
 
       instance_types = ["t2.micro"]
       capacity_type  = "ON_DEMAND"
