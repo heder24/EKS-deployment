a0b87efc6406fd3cf2bef46f7d859d4990f04241
heder redon <hederredon@gmail.com>
2023-12-01 10:43:22 -0500

    Added csi module

--- main.tf
+++ main.tf
@@ -102,8 +102,8 @@ module "csi" {
   source  = "app.terraform.io/heder24/csi/aws"
   version = "1.0.0"
   # aws_profile      = "bigdata-labs"
-  aws_region       = var.region
-  eks_cluster_name = var.cluster_name
+  aws_region       = "us-east-2"
+  eks_cluster_name = "prod"
   }
 
 ###############################################################################
