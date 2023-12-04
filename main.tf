e0a6047307ac7e472e6d385773f821e36fdf9d0a
heder redon <hederredon@gmail.com>
2023-12-04 13:34:26 -0500

    Added webhook sg andchanged destroy to false

--- main.tf
+++ main.tf
@@ -51,21 +51,6 @@ module "eks" {
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
-
-###############################################################
-# SG to webhook of AWS load balancer 
-###############################################################
-node_security_group_additional_rules = {
-  ingress_allow_access_from_control_plane = {
-    type                          = "ingress"
-    protocol                      = "tcp"
-    from_port                     = 9443
-    to_port                       = 9443
-    source_cluster_security_group = true
-    description                   = "Allow access from control plane to webhook port of AWS load balancer controller"
-  }
-}
-
   # aws-auth configmap
   manage_aws_auth_configmap = true
   aws_auth_users = [
