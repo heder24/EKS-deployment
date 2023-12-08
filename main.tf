39f7b320366d7415d40bcb2d85d977b6c22fa015
heder redon <hederredon@gmail.com>
2023-12-07 20:20:25 -0500

    changed destroy command to false

--- main.tf
+++ main.tf
@@ -47,16 +47,7 @@ module "eks" {
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
 
-node_security_group_additional_rules = {
-    ingress_allow_access_from_control_plane = {
-      type                          = "ingress"
-      protocol                      = "tcp"
-      from_port                     = 9443
-      to_port                       = 9443
-      source_cluster_security_group = true
-      description                   = "Allow access from control plane to webhook port of AWS load balancer controller"
-    }
-  }
+
   # aws-auth configmap
   manage_aws_auth_configmap = true
   aws_auth_users = [
