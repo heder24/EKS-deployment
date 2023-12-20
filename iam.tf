16fd3bfc58b0b371440fd81c772008d971dc6b40
heder redon <hederredon@gmail.com>
2023-12-19 22:22:14 -0500

    changed destroy command to false and updated iam file to add a user

--- iam.tf
+++ iam.tf
@@ -34,6 +34,19 @@ module "eks_admins_iam_role" {
   ]
 }
 
+
+module "eks-user_iam_user" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  version = "5.33.0"
+
+  name                          = "eks-user"
+  create_iam_access_key         = false
+  create_iam_user_login_profile = false
+
+  force_destroy = true
+}
+
+
 module "allow_assume_eks_admins_iam_policy" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
   version = "5.33.0"
@@ -61,6 +74,6 @@ module "eks_admins_iam_group" {
   name                              = "eks-admin"
   attach_iam_self_management_policy = false
   create_group                      = true
-  group_users                       = [var.username]
+  group_users                       = [var.username,module.eks-user_iam_user.iam_user_name ]
   custom_group_policy_arns          = [module.allow_assume_eks_admins_iam_policy.arn]
 }
