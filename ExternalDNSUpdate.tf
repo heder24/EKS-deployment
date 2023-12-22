3825767e6fa53bf772a0b59e729ee366b09a51d1
heder redon <hederredon@gmail.com>
2023-12-21 21:17:46 -0500

    changed destroy command to false and added external dns role and service account

---
+++ ExternalDNSUpdate.tf
@@ -0,0 +1,65 @@
+data "aws_iam_policy_document" "external_dns_role_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(module.eks.oidc_provider, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:external_dns_role"]
+    }
+
+    principals {
+      identifiers = [module.eks.oidc_provider_arn]
+      type        = "Federated"
+    }
+  }
+}
+resource "aws_iam_role" "external_dns_role" {
+  name = "ExternalDNSUpdateRole"
+  
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "external_dns_policy" {
+  name        = "ExternalDNSUpdatePolicy"
+  description = "Policy for allowing external DNS updates"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "route53:ChangeResourceRecordSets",
+        "route53:GetChange",
+        "route53:ListResourceRecordSets"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  policy_arn = aws_iam_policy.external_dns_policy.arn
+  role       = aws_iam_role.external_dns_role.name
+}
+output "external_dns_role_arn" {
+  value = aws_iam_role.external_dns_role.arn
+}
---
+++ helm-externalDNS.tf
@@ -0,0 +1,47 @@
+# provider "helm" {
+#   kubernetes {
+#     host                   = aws_eks_cluster.module.eks.cluster_endpoint
+#     cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+#     exec {
+#       api_version = "client.authentication.k8s.io/v1beta1"
+#       args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
+#       command     = "aws"
+#     }
+#   }
+# }
+
+resource "helm_release" "external-dns" {
+  name = "external-dns"
+  repository = "https://kubernetes-sigs.github.io/external-dns/"
+  chart      = "external-dns"
+  namespace  = "kube-system"
+#   version    = "1.4.1"
+timeout = 600  # Set a higher timeout value in seconds
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+
+  # set {
+  #   name  = "image.tag"
+  #   value = "latest"
+  # }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "external-dns"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.external_dns_role.arn
+  }
+
+  depends_on = [
+    module.eks.eks_managed_node_groups,
+    aws_iam_role_policy_attachment.external_dns_attach
+  ]
+
+  
+}
