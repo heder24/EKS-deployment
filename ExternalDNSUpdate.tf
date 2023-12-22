943ddd4534561f3f3e905a85f6a5e59877227aad
heder redon <hederredon@gmail.com>
2023-12-21 22:53:42 -0500

    changed destroy command to false and added external dns role and service account

--- ExternalDNSUpdate.tf
+++ ExternalDNSUpdate.tf
@@ -34,7 +34,7 @@ data "aws_iam_policy_document" "external_dns_role_assume_role_policy" {
 # EOF
 # }
 resource "aws_iam_role" "external_dns_role" {
-  assume_role_policy = data.aws_iam_policy_document.external_dns_role_assume_role_policy_assume_role_policy.json
+  assume_role_policy = data.aws_iam_policy_document.external_dns_assume_role_policy.json
   name               = "external-dns"
 }
 
