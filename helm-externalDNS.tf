2053afa93143486a68b5153e61cf31e66831a332
heder redon <hederredon@gmail.com>
2023-12-21 22:23:30 -0500

    changed destroy command to false and added external dns role and service account

--- helm-externalDNS.tf
+++ helm-externalDNS.tf
@@ -15,7 +15,7 @@ resource "helm_release" "external-dns" {
   repository = "https://kubernetes-sigs.github.io/external-dns/"
   chart      = "external-dns"
   namespace  = "kube-system"
-  timeout = 600  # Set a higher timeout value in seconds
+timeout = 1200  # Set a higher timeout value in seconds
 
   set {
     name  = "clusterName"
