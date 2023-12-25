5a89fe88c57d2dc868b50eef47272d818a636946
heder redon <hederredon@gmail.com>
2023-12-24 19:58:51 -0500

    Modified external-dns helm file and destroy to true

--- helm-externalDNS.tf
+++ helm-externalDNS.tf
@@ -1,75 +1,75 @@
 
-resource "helm_release" "external-dns" {
-  name = "external-dns"
-  repository = "https://kubernetes-sigs.github.io/external-dns/"
-  chart      = "external-dns"
-  namespace  = "kube-system"
-  create_namespace = true
-  # timeout = 600 # Set a higher timeout value in seconds
+# resource "helm_release" "external-dns" {
+#   name = "external-dns"
+#   repository = "https://kubernetes-sigs.github.io/external-dns/"
+#   chart      = "external-dns"
+#   namespace  = "kube-system"
+#   create_namespace = true
+#   # timeout = 600 # Set a higher timeout value in seconds
 
-    set {
-    name  = "wait-for"
-     value = aws_iam_role.external_dns_role.arn
-  }
-
-  set {
-    name  = "clusterName"
-    value = module.eks.cluster_name
-  }
-
-  set {
-    name  = "serviceAccount.name"
-    value = "external-dns"
-  }
-  #   set {
-  #   name  = "rbac.create"
-  #   value = "true"
-  # }
-
-  # set {
-  #   name  = "rbac.serviceAccountName"
-  #   value = "external-dns"
-  # }
-
-#  set {
-#     name  = "source"
-#     value = "service,ingress"
+#     set {
+#     name  = "wait-for"
+#      value = aws_iam_role.external_dns_role.arn
 #   }
 
-  set {
-    name  = "domainFilter"
-    value = "hederdevops.com"  # Adjust the domain filter as needed
-  }
+#   set {
+#     name  = "clusterName"
+#     value = module.eks.cluster_name
+#   }
 
-  set {
-    name  = "provider"
-    value = "aws"
-  }
+#   set {
+#     name  = "serviceAccount.name"
+#     value = "external-dns"
+#   }
+#   #   set {
+#   #   name  = "rbac.create"
+#   #   value = "true"
+#   # }
 
-  set {
-    name  = "policy"
-    value = "upsert-only"
-  }
+#   # set {
+#   #   name  = "rbac.serviceAccountName"
+#   #   value = "external-dns"
+#   # }
 
-  set {
-    name  = "awsZoneType"
-    value = "public"
-  }
+# #  set {
+# #     name  = "source"
+# #     value = "service,ingress"
+# #   }
+
+#   set {
+#     name  = "domainFilter"
+#     value = "hederdevops.com"  # Adjust the domain filter as needed
+#   }
+
+#   set {
+#     name  = "provider"
+#     value = "aws"
+#   }
+
+#   set {
+#     name  = "policy"
+#     value = "upsert-only"
+#   }
+
+#   set {
+#     name  = "awsZoneType"
+#     value = "public"
+#   }
 
 
-  set {
-    name  = "txtOwnerId"
-    value = data.aws_route53_zone.hosted_zone.id
-  }
+#   set {
+#     name  = "txtOwnerId"
+#     value = data.aws_route53_zone.hosted_zone.id
+#   }
 
 
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = aws_iam_role.external_dns_role.arn
-  }
+#   set {
+#     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+#     value = aws_iam_role.external_dns_role.arn
+#   }
 
-  depends_on = [
-    module.eks.eks_managed_node_groups,
-    aws_iam_role_policy_attachment.external_dns_attach_policy
-  ] 
-}
+#   depends_on = [
+#     module.eks.eks_managed_node_groups,
+#     aws_iam_role_policy_attachment.external_dns_attach_policy
+#   ] 
+# }
