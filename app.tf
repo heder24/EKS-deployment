3cdb2e92e20f118b85d61d388a6bfbf6407d4fc1
heder redon <hederredon@gmail.com>
2023-12-27 14:07:41 -0500

    changed destroy command false

--- app.tf
+++ app.tf
@@ -2,8 +2,8 @@ resource "helm_release" "knote" {
   name = "knote"
   repository = "/home/cyber/repos/EKS-deployment/knote"
   chart      = "knote"
-#   namespace  = "knote-app"
-#   create_namespace = true
+  namespace  = "knote-app"
+  create_namespace = true
   
    depends_on = [
     module.eks.eks_managed_node_groups,
