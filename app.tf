c40d8a6b9b307cef1185e8db1c1062d07ee6f4be
heder redon <hederredon@gmail.com>
2023-12-26 21:44:07 -0500

    Added helm chart for app and changed destroy command false

--- app.tf
+++ app.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "knote" {
   name = "knote"
-  repository = "/home/cyber/repos/EKS-deployment/knote"
+  repository = "/home/cyber/repos/EKS-deployment/knote/*"
   chart      = "knote"
   namespace  = "knote-app"
   create_namespace = true
