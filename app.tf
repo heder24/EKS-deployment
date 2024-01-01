d32678d67169792ea32e7856c58735ea155c6665
heder redon <hederredon@gmail.com>
2024-01-01 17:35:36 -0500

    changed destroy to false

--- app.tf
+++ app.tf
@@ -1,13 +1,13 @@
-resource "helm_release" "knote" {
-  name             = "knote"
-  repository       = "/home/cyber/repos/EKS-deployment/knote"
-  chart            = "knote"
-  namespace        = "knote-app"
-  create_namespace = true
+# resource "helm_release" "knote" {
+#   name             = "knote"
+#   repository       = "/home/cyber/repos/EKS-deployment/knote"
+#   chart            = "knote"
+#   namespace        = "knote-app"
+#   create_namespace = true
 
-  depends_on = [
-    module.eks.eks_managed_node_groups,
-  ]
-}
+#   depends_on = [
+#     module.eks.eks_managed_node_groups,
+#   ]
+# }
 
 
