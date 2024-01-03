17e87361f629feb7d3643f71971a6f1e2bd36d00
heder redon <hederredon@gmail.com>
2024-01-03 14:55:49 -0500

    commented out external dns

--- prometheus.tf
+++ prometheus.tf
@@ -8,5 +8,26 @@ resource "helm_release" "prometheus" {
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "prometheus"
    namespace = "monitoring"
+
+  set {
+    name  = "server.service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "server.ingress.enabled"
+    value = "false"
+  }
+
+  set {
+    name  = "server.servicePort"
+    value = 9090
+  }
+
+  set {
+    name  = "alertmanager.enabled"
+    value = "true"
+  }
 }
+
 
