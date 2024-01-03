ee586d459dc4cfcb59fa76bdf6602edd3a829b20
heder redon <hederredon@gmail.com>
2024-01-03 14:22:17 -0500

    changed destroy to false

--- grafana.tf
+++ grafana.tf
@@ -4,4 +4,20 @@ resource "helm_release" "grafana" {
   repository = "https://grafana.github.io/helm-charts"
   chart      = "grafana"
     namespace = "monitoring"
+
+
+  set {
+    name  = "service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
+    name  = "ingress.enabled"
+    value = "false"
+  }
+
+  set {
+    name  = "service.port"
+    value = 3000
+  }
 }
