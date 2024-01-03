cd60d87e6558c3418f3a1a46235d68eea9a76baa
heder redon <hederredon@gmail.com>
2024-01-02 20:49:35 -0500

    changed destroy to false

---
+++ get-cert.tf
@@ -0,0 +1,29 @@
+
+resource "null_resource" "acm_certificate" {
+  triggers = {
+    domain_name = var.prod_domain_name
+  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      domain_name="${var.prod_domain_name}"
+
+      certificate_arn=$(aws acm list-certificates \
+        --query "CertificateSummaryList[?DomainName=='$domain_name'].CertificateArn" \
+        --output text)
+
+      echo "ACM Certificate ARN: $certificate_arn"
+      terraform output certificate_arn -- "$certificate_arn"  # Output the ARN as a Terraform output variable
+    EOT
+  }
+}
+
+output "certificate_arn" {
+  value = null_resource.acm_certificate.triggers["certificate_arn"]
+}
+
+variable "prod_domain_name" {
+  description = "Domain name for ACM certificate"
+  type        = string
+  default     = "www.hederdevops.com"
+}
