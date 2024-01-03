d0b75a7ea8ff3dd8841c74fa509ef3dd7c1a72ac
heder redon <hederredon@gmail.com>
2024-01-03 09:30:08 -0500

    added prometheus and grafana resources

--- grafana.tf
+++ grafana.tf
@@ -4,26 +4,7 @@ resource "helm_release" "grafana" {
   repository = "https://grafana.github.io/helm-charts"
   chart      = "grafana"
     namespace = "monitoring"
-  set {
-    name  = "adminPassword"
-    value = "your-grafana-admin-password"  # Replace with your desired Grafana admin password
-  }
 
-  set {
-    name  = "datasources."  # Adjust datasource configuration as needed
-    value = jsonencode([
-      {
-        name                  = "Prometheus"
-        type                  = "prometheus"
-        access_mode           = "proxy"
-        url                   = "http://prometheus-server.monitoring.svc:80"
-        is_default            = true
-        version               = 1
-        editable              = true
-        jsonData               = {
-          tlsAuth           = false
-        }
-      },
-    ])
-  }
+
+  
 }
