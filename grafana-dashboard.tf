2224337a43b00dbda0f97f279b5f8737da53dc90
heder redon <hederredon@gmail.com>
2024-01-06 23:32:45 -0500

    changed destroy to false

--- grafana-dashboard.tf
+++ grafana-dashboard.tf
@@ -1,40 +1,40 @@
 
-resource "grafana_dashboard" "cluster_monitoring" {
+# resource "grafana_dashboard" "cluster_monitoring" {
 
-  config_json = <<EOF
-{
-  "editable": true,
-  "panels": [
-    {
-      "type": "graph",
-      "title": "CPU Usage",
-      "targets": [
-        {
-          "refId": "A",
-          "expr": "sum(cluster_cpu_usage)"
-        }
-      ]
-    },
-    {
-      "type": "graph",
-      "title": "Memory Usage",
-      "targets": [
-        {
-          "refId": "A",
-          "expr": "sum(cluster_memory_usage)"
-        }
-      ]
-    }
+#   config_json = <<EOF
+# {
+#   "editable": true,
+#   "panels": [
+#     {
+#       "type": "graph",
+#       "title": "CPU Usage",
+#       "targets": [
+#         {
+#           "refId": "A",
+#           "expr": "sum(cluster_cpu_usage)"
+#         }
+#       ]
+#     },
+#     {
+#       "type": "graph",
+#       "title": "Memory Usage",
+#       "targets": [
+#         {
+#           "refId": "A",
+#           "expr": "sum(cluster_memory_usage)"
+#         }
+#       ]
+#     }
    
-  ],
-  "time": {
-    "from": "now-6h",
-    "to": "now"
-  },
-  "refresh": "10s"
-}
-EOF
-}
+#   ],
+#   "time": {
+#     "from": "now-6h",
+#     "to": "now"
+#   },
+#   "refresh": "10s"
+# }
+# EOF
+# }
 
 
 
