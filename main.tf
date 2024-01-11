abacd6771d083c17d6450bef986c5b9e3c69bda3
heder redon <hederredon@gmail.com>
2024-01-10 21:39:45 -0500

    Re-added external dns annotation to ingress manifest file

--- main.tf
+++ main.tf
@@ -93,9 +93,9 @@ module "eks" {
 
   eks_managed_node_groups = {
     prod = {
-      min_size     = 3
+      min_size     = 1
       max_size     = 10
-      desired_size = 3
+      desired_size = 2
 
       instance_types = ["t3.large"]
       capacity_type  = "ON_DEMAND"
