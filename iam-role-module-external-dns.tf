b59b586a6d05a4e8f61bcac542acc9df26e343cc
heder redon <hederredon@gmail.com>
2024-02-02 22:52:38 -0500

    Changed destroy to false

--- iam-role-module-external-dns.tf
+++ iam-role-module-external-dns.tf
@@ -1,15 +1,15 @@
-module "iam_iam-role-for-service-accounts-eks" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "5.33.0"
-  role_name                     = "external-dns"
-  attach_external_dns_policy    = true
-  external_dns_hosted_zone_arns = data.aws_route53_zone.hosted_zone.arn # Lab HostedZone
+# module "iam_iam-role-for-service-accounts-eks" {
+#   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+#   version = "5.33.0"
+#   role_name                     = "external-dns"
+#   attach_external_dns_policy    = true
+#   external_dns_hosted_zone_arns = data.aws_route53_zone.hosted_zone.arn # Lab HostedZone
 
-  oidc_providers = {
-    eks = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:external-dns"]
-    }
-  }
+#   oidc_providers = {
+#     eks = {
+#       provider_arn               = module.eks.oidc_provider_arn
+#       namespace_service_accounts = ["kube-system:external-dns"]
+#     }
+#   }
 
-}
+# }
