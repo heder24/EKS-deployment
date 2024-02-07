dbb637f985682a29633d877632384efeeb74c586
heder redon <hederredon@gmail.com>
2024-02-07 15:31:22 -0500

    added csi secrets store intial files

--- csi_secrets.tf
+++ csi_secrets.tf
@@ -2,7 +2,7 @@ resource "helm_release" "csi" {
   name       = "csi-secrets-store"
   chart      = "secrets-store-csi-driver"
   repository = "https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts"
-  
+  namespace = "kube-system"  
   set {
     name  = "grpcSupportedProviders"
     value = "aws"
--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "heder24"
 
     workspaces {
-      name = "knttt-wkspace"
+      name = "knttee-wkspace"
     }
   }
 }
