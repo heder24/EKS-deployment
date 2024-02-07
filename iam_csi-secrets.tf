4086c1f8d847754db9f7f0ea021d699446806105
heder redon <hederredon@gmail.com>
2024-02-07 11:51:28 -0500

    added csi secrets store intial files

--- iam_csi-secrets.tf
+++ iam_csi-secrets.tf
@@ -16,44 +16,27 @@ data "aws_iam_policy_document" "csi-store-driver-sa_role_assume_role_policy" {
   }
 }
 
-# resource "aws_iam_role" "secret_store_CSI_driver_role" {
-#   assume_role_policy = data.aws_iam_policy_document.external_dns_role_assume_role_policy.json
-#   name               = "secret_store_CSI_driver"
-# }
+resource "aws_iam_role" "secret_store_CSI_driver_role" {
+  assume_role_policy = data.aws_iam_policy_document.external_dns_role_assume_role_policy.json
+  name               = "csi-store-driver-sa"
+}
 
 resource "aws_iam_policy" "secret_store_CSI_driver_policy" {
   policy = file("./iam_secret_store_CSI_driver.json")
   name   = "csi-store-driver-sa"
 }
 
-# resource "aws_iam_role_policy_attachment" "iam_secret_store_CSI_driver_attach_policy" {
-#   policy_arn = aws_iam_policy.secret_store_CSI_driver_policy.arn
-#   role       = aws_iam_role.secret_store_CSI_driver_role.name
-# }
-# output "external_dns_role_arn" {
-#   value = aws_iam_role.secret_store_CSI_driver_role.arn
-# }
-
-
-module "secret_store_CSI_driver_role" {
- source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
-  version = "5.34.0"
-
-  role_name = "csi-store-driver-sa"
-  allow_self_assume_role = true
-  
-  oidc_providers = {
-    ex = {
-      provider_arn               = module.eks.oidc_provider_arn
-      namespace_service_accounts = ["kube-system:csi-store-driver-sa"]
-    }
-
-
-  role_policy_arns = [aws_iam_policy.secret_store_CSI_driver_policy.arn]
-  
-}
+resource "aws_iam_role_policy_attachment" "iam_secret_store_CSI_driver_attach_policy" {
+  policy_arn = aws_iam_policy.secret_store_CSI_driver_policy.arn
+  role       = aws_iam_role.secret_store_CSI_driver_role.name
 }
 output "csi-store-driver-sa" {
-  value = module.secret_store_CSI_driver_role.iam_role_arn  
-  }
+  value = aws_iam_role.secret_store_CSI_driver_role.arn
+}
 
+resource "kubernetes_service_account" "csi_store_driver_sa" {
+  metadata {
+    name      = "csi-store-driver-sa"
+    namespace = "kube-system"  # You may adjust the namespace as needed
+  }
+}
