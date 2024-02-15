4e0d25aa9f7d82e511b77b665ee01683331dbc99
heder redon <hederredon@gmail.com>
2024-02-15 14:03:16 -0500

    changed terraform workspace name

--- iam_csi-secrets.tf
+++ iam_csi-secrets.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "knote_role_assume_role_policy" {
+data "aws_iam_policy_document" "secret_store_CSI_driver_role_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRoleWithWebIdentity"]
     effect  = "Allow"
@@ -6,7 +6,7 @@ data "aws_iam_policy_document" "knote_role_assume_role_policy" {
     condition {
       test     = "StringEquals"
       variable = "${replace(module.eks.oidc_provider, "https://", "")}:sub"
-      values   = ["system:serviceaccount:knote-app:knote"]
+      values   = ["system:serviceaccount:knote-app:secret_store_CSI_driver"]
     }
 
     principals {
@@ -17,41 +17,41 @@ data "aws_iam_policy_document" "knote_role_assume_role_policy" {
 }
 
 resource "aws_iam_role" "secret_store_CSI_driver_role" {
-  assume_role_policy = data.aws_iam_policy_document.knote_role_assume_role_policy.json
-  name               = "knote"
+  assume_role_policy = data.aws_iam_policy_document.secret_store_CSI_driver_role_assume_role_policy.json
+  name               = "csi-store-driver-sa"
 }
 
 resource "aws_iam_policy" "secret_store_CSI_driver_policy" {
   policy = file("./iam_secret_store_CSI_driver.json")
-  name   = "knote"
+  name   = "csi-store-driver-sa"
 }
 
 resource "aws_iam_role_policy_attachment" "iam_secret_store_CSI_driver_attach_policy" {
   policy_arn = aws_iam_policy.secret_store_CSI_driver_policy.arn
   role       = aws_iam_role.secret_store_CSI_driver_role.name
 }
-output "knote" {
+output "secret_store_CSI_driver" {
   value = aws_iam_role.secret_store_CSI_driver_role.arn
 }
 
 
-# resource "kubernetes_service_account" "csi_sa_service_account" {
-#   metadata {
-#     name      = "knote"
-#     namespace = "knote-app"
-#     annotations = {
-#       "eks.amazonaws.com/role-arn" = aws_iam_role.secret_store_CSI_driver_role.arn
-#     }
+resource "kubernetes_service_account" "csi_sa_service_account" {
+  metadata {
+    name      = "csi-store-driver-sa"
+    namespace = "knote-app"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.secret_store_CSI_driver_role.arn
+    }
   
-#   }
-#   automount_service_account_token = true
+  }
+  automount_service_account_token = true
 
-#   depends_on = [
-#     aws_iam_role_policy_attachment.iam_secret_store_CSI_driver_attach_policy,
-#     aws_iam_role.secret_store_CSI_driver_role,
-#     kubernetes_namespace.knote_app
+  depends_on = [
+    aws_iam_role_policy_attachment.iam_secret_store_CSI_driver_attach_policy,
+    aws_iam_role.secret_store_CSI_driver_role,
+    kubernetes_namespace.kubernetes_namespace.knote_app
     
-#   ]
-# }
+  ]
+}
 ########################################################
 
--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "heder24"
 
     workspaces {
-      name = "kNeeTe-wkspace"
+      name = "kNffTe-wkspace"
     }
   }
 }
