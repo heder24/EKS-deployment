9a53ae000fa0001d9e3da20144f31f19d3524b37
heder redon <hederredon@gmail.com>
2024-02-21 08:09:04 -0500

    changed terraform workspace name

--- sealed-secrets.tf
+++ sealed-secrets.tf
@@ -1,9 +1,8 @@
 resource "helm_release" "sealed_secrets" {
   name       = "sealed-secrets"
   namespace  = "kube-system"
-  repository = "https://github.com/bitnami-labs/sealed-secrets"
+  repository = "https://bitnami-labs.github.io/sealed-secrets"
   chart      = "sealed-secrets"
-
   set {
     name  = "fullnameOverride"
     value = "sealed-secrets-controller"
