05aa551780307429fdb5a71f4ef0f0c8ce6088fa
heder redon <hederredon@gmail.com>
2024-02-26 18:00:28 -0500

    changed workspace name

--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "heder24"
 
     workspaces {
-      name = "kNorte-work-space"
+      name = "kNoste-work-space"
     }
   }
 }
--- sealed-secrets.tf
+++ sealed-secrets.tf
@@ -5,4 +5,9 @@ resource "helm_release" "sealed_secrets" {
   repository = "https://bitnami-labs.github.io/sealed-secrets"
   chart      = "sealed-secrets"
   version    = "2.15.0"
+
+  set {
+    name  = "controller.name"
+    value = "sealed-secrets-controller"
+  }
 }
